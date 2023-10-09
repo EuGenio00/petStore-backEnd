70ae320899b363185a8b611fddca6ccf4e3134b3
EuGenio00 <eugeniotchanjongombe10@gmail.com>
2023-10-09 18:00:33 -0300

    atualizando a entidade Address

--- src/main/java/com/senai/pets/entities/Address.java
+++ src/main/java/com/senai/pets/entities/Address.java
@@ -3,12 +3,14 @@ package com.senai.pets.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-
-@Embeddable
+@AllArgsConstructor
 public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String street;
   private String number;
   private String state;
